903732f1e0c821ab28b531e6ebab998053b832bb
Harmandeep Mand <mr.mand@Harmandeeps-MacBook-Air.local>
2020-08-15 11:32:43 -0400

    info page animation updated

--- src/pages/Home/Info.js
+++ src/pages/Home/Info.js
@@ -1,37 +1,73 @@
 import React,{ useState, useEffect } from 'react';
-import { SafeAreaView, Text, StyleSheet, View, TouchableOpacity } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, View, TouchableOpacity, Animated, Easing } from 'react-native';
 
 const Info = () => {
   const[screen1,setscreen1] = useState(true)
   const[screen2,setscreen2] = useState(false)
   const[screen3,setscreen3] = useState(false)
+  const[sonefade,setonefade] = useState(1)
   const[timer,resettimer] = useState(2000)
+
+  let scaleValue = new Animated.Value(0);
+  let fadeValue = new Animated.Value(1)
+  const textScale = scaleValue.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [1, 1.1, 1.2]
+  });
+  let zoom = { transform: [{ scale: textScale }] };
 
   setTimeout(()=>{
     if(screen1 == true){
-      setscreen1(false)
-      setscreen2(true)
-      resettimer(2000)
+      Animated.timing(scaleValue, {
+        toValue: 14,
+        duration: 350,
+        easing: Easing.linear,
+        useNativeDriver: true
+      }).start();
+      setTimeout(()=>{
+        setscreen1(false)
+        setscreen2(true)
+        resettimer(2000)
+      },350)
     }else if(screen2 == true){
-      setscreen2(false)
-      setscreen3(true)
-      resettimer(2000)
+      Animated.timing(scaleValue, {
+        toValue: 14,
+        duration: 350,
+        easing: Easing.linear,
+        useNativeDriver: true
+      }).start();
+      setTimeout(()=>{
+        setscreen2(false)
+        setscreen3(true)
+        resettimer(2000)
+      },350)
     }else if(screen3 == true){
-      setscreen3(false)
-      setscreen1(true)
-      resettimer(2000)
+      Animated.timing(scaleValue, {
+        toValue: -2,
+        duration: 350,
+        easing: Easing.linear,
+        useNativeDriver: true
+      }).start();
+      setTimeout(()=>{
+        setscreen3(false)
+        setscreen1(true)
+        resettimer(2000)
+      },350)
     }
   },timer)
 
   return(
     <View style={Styles.Page}>
-        {screen1 == true ? <Text style={Styles.Text}>Welcome</Text> : null}
-        {screen2 == true ? <Text style={Styles.Text}>To</Text> : null}
-        {screen3 == true ? <View style={{justifyContent:'center',alignItems:'center'}}>
-          <Text style={Styles.Text}>Harmandeep Mand</Text>
-          <TouchableOpacity style={{borderWidth:1,width:300,borderColor:'white'}}/>
-          <Text style={{fontSize:25, color:'white'}}>React Native Test App</Text>
-        </View> : null}
+        {screen1 == true ? <Animated.View style={zoom}><Text style={{color:'white',fontSize:30}}>Welcome</Text></Animated.View> : null}
+        {screen2 == true ? <Animated.View style={zoom}><Text style={{color:'white',fontSize:30}}>To</Text></Animated.View> : null}
+        {screen3 == true ? 
+          <Animated.View style={zoom}>
+            <View style={{justifyContent:'center',alignItems:'center'}}>
+              <Text style={{color:'white',fontSize:30}}>Harmandeep Mand</Text>
+              <TouchableOpacity style={{borderWidth:1,width:300,borderColor:'white'}}/>
+              <Text style={{fontSize:25, color:'white'}}>React Native Test App</Text>
+            </View>
+          </Animated.View> : null}
     </View>
   )
 }
@@ -43,10 +79,6 @@ const Styles = StyleSheet.create({
     justifyContent:'center',
     alignItems:'center',
     backgroundColor:'#5810d8'
-  },
-  Text:{
-    color:'white',
-    fontSize:30
   }
 })
 
